5c336ec5df40dcd0560d0f815410193c48a26507
Ivan <pit.sedow2014@yandex.ru>
2023-07-19 08:03:33 +0500

    tests

--- src/test/java/ru/practicum/shareit/item/model/CommentTest.java
+++ src/test/java/ru/practicum/shareit/item/model/CommentTest.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
